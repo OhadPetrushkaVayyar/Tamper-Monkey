d210746cb240faf781297458f48ab45c38b7debe
Ohad Petrushka <47821404+OhadPetrushkaVayyar@users.noreply.github.com>
2019-03-04 11:34:07 +0200

    Initial

---
+++ m2Walabot_com.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         SkipGeoIP Javascript
+// @namespace    http://walabot.com
+// @version      0.1
+// @description  Simple Script to automatically skip geoip restrction on store.walabot.com
+// @author       Ohad Petrushka
+// @match        https://store.walabot.com/*
+// @match        http://store.walabot.com/*
+// @match        https://uatstore.walabot.com/*
+// @match        http://uatstore.walabot.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.cookie="skipgeoip=1";
+})();
